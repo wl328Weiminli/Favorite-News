3f10e98077fb208dffb50eb37904b17ee39c20ca
derek <wl328@njit.edu>
2020-12-12 13:21:29 -0500

    Initial commit

---
+++ app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -0,0 +1,45 @@
+package com.laioffer.tinnews.model;
+
+import java.util.Objects;
+
+public class Article {
+    public String author;
+    public String content;
+    public String description;
+    public String publishedAt;
+    public String title;
+    public String url;
+    public String urlToImage;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(author, article.author) &&
+                Objects.equals(content, article.content) &&
+                Objects.equals(description, article.description) &&
+                Objects.equals(publishedAt, article.publishedAt) &&
+                Objects.equals(title, article.title) &&
+                Objects.equals(url, article.url) &&
+                Objects.equals(urlToImage, article.urlToImage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, content, description, publishedAt, title, url, urlToImage);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                ", description='" + description + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
+                '}';
+    }
+}
